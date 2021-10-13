727e7c11e3b3eefe45c74d8efd3b1b044ce36401
mfittko <manuel.fittko@sofatutor.com>
2021-10-13 10:04:26 +0200

    add http basic auth and forward to target host

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 set -eu
 
-envsubst '${NGINX_HOST} ${BACKEND_PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+echo 'Creating .htpasswd file...'
+(apt-get update && apt-get install -y apache2-utils && apt-get clean) > /dev/null
+htpasswd -cb /etc/nginx/conf.d/nginx.htpasswd "$HTTP_BASIC_USER" "$HTTP_BASIC_PASS"
+envsubst '${NGINX_HOST} ${BACKEND_PORT} ${PROXY_REALM}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 exec "$@"
